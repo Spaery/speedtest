a24f9da95105220a7fe45a99ca4076b2dd95a8ed
Spaery <bowserrocks1230@gmail.com>
2022-12-10 22:00:56 -0600

    Updated script for newest version of InfluxDB
    The previous curl script was depreciated and now requires different headers and authentication to write data to a database

--- speedtest.sh
+++ speedtest.sh
@@ -4,6 +4,7 @@ LOOP="${LOOP:-false}"
 LOOP_DELAY="${LOOP_DELAY:-60}"
 DB_SAVE="${DB_SAVE:-false}"
 DB_HOST="${DB_HOST:-http://localhost:8086}"
+DB_ORG="${DB_ORG:-speedtest}"
 DB_NAME="${DB_NAME:-speedtest}"
 DB_USERNAME="${DB_USERNAME:-admin}"
 DB_PASSWORD="${DB_PASSWORD:-password}"
@@ -16,23 +17,27 @@ run_speedtest()
     # Start speed test
     echo "Running a Speed Test..."
     JSON=$(speedtest --accept-license --accept-gdpr -f json)
-    DOWNLOAD="$(echo $JSON | jq -r '.download.bandwidth')"
-    UPLOAD="$(echo $JSON | jq -r '.upload.bandwidth')"
-    PING="$(echo $JSON | jq -r '.ping.latency')"
-    echo "Your download speed is $(($DOWNLOAD  / 125000 )) Mbps ($DOWNLOAD Bytes/s)."
-    echo "Your upload speed is $(($UPLOAD  / 125000 )) Mbps ($UPLOAD Bytes/s)."
+    DOWNLOAD="$(((echo $JSON | jq -r '.download.bandwidth') * 8))" # Download Speed in bits/sec
+    UPLOAD="$(((echo $JSON | jq -r '.upload.bandwidth') * 8))" # Upload Speed in bits/ sec
+    PING="$(echo $JSON | jq -r '.ping.latency')" # Latency in milliseconds
+    echo "Your download speed is $(($DOWNLOAD  / 125000 )) Mbps ($(( $DOWNLOAD / 8 )) Bytes/s)."
+    echo "Your upload speed is $(($UPLOAD  / 125000 )) Mbps ($(( $UPLOAD / 8 )) Bytes/s)."
     echo "Your ping is $PING ms."
 
     # Save results in the database
     if $DB_SAVE; 
     then
         echo "Saving values to database..."
-        curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
-            --data-binary "download,host=$HOSTNAME value=$DOWNLOAD $DATE"
-        curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
-            --data-binary "upload,host=$HOSTNAME value=$UPLOAD $DATE"
-        curl -s -S -XPOST "$DB_HOST/write?db=$DB_NAME&precision=s&u=$DB_USERNAME&p=$DB_PASSWORD" \
-            --data-binary "ping,host=$HOSTNAME value=$PING $DATE"
+        curl --request POST \
+		"http://$DB_HOST/api/v2/write?org=$DB_ORG&bucket=$DB_NAME&precision=ns" \
+  		--header "Authorization: Token $API_TOKEN" \
+  		--header "Content-Type: text/plain; charset=utf-8" \
+  		--header "Accept: application/json" \
+  		--data-binary "
+ 			download,host=$HOSTNAME value=$DOWNLOAD
+   			upload,host=$HOSTNAME value=$UPLOAD
+   			ping,host=$HOSTNAME value=$PING
+    		"
         echo "Values saved."
     fi
 }
